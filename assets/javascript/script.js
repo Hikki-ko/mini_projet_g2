2923864e6eecd1e562b3f03e7f4221409e5f7601
Hikki-ko <arcobalenosensei@gmail.com>
2025-10-22 17:39:17 +0400

    Ajout de la clé d'API. Ajout de pseudo-code pour faciliter la compréhension de l'interaction.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,15 @@
+// Déclarations variables
+const API_KEY =
+  "63c89bc4a68f3733d17ec87750e68ab04de872ba71f88f7703f2ea0161502834";
+
 /**************
  *	On ajoute un évènement "lorsque l'arborescence DOM est chargée".
  *   Ainsi, nous sommes certains de manipuler des élements
  *	chargés et existants dans notre DOM.
  **************/
-window.addEventListener("DOMContentLoaded", (event) => {});
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Pseudo-code !!!
+  const nombre_questions = "valeur récupérée dans l'HTML";
+  const liste_villes =
+    "https://api.openaq.org/v3/locations?limit=${nombre_questions}";
+});
