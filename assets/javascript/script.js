b307ee38a95865bbbe4f320a23174b37b51f9112
Zoé <zoe.van-de-moortele@etu.unilim.fr>
2025-10-23 19:57:19 +0200

    Début de JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,7 +9,38 @@ const API_KEY =
  **************/
 window.addEventListener("DOMContentLoaded", (event) => {
   // Pseudo-code !!!
-  const nombre_questions = "valeur récupérée dans l'HTML";
-  const liste_villes =
+  //const nombre_questions = "valeur récupérée dans l'HTML";
+  //const liste_villes =
     "https://api.openaq.org/v3/locations?limit=${nombre_questions}";
+
+  //On récupère les éléments du DOM
+  const confirmButton = document.querySelector('.button');
+  const cityItems=document.querySelectorAll('.city_item');
+  const correctScoreElement = document.getElementById('correctScore');
+  const totalQuestionsElement = document.getElementById('totalQuestions');
+
+  //On définit les données du jeu
+  let correctScore = 0;
+  let totalQuestions = 10;
+  let currentQuestion = 0;
+  let cityData = []; //pour stocker les données des villes de l'API
+
+  function initGame(){
+    correctScore = 0;
+    currentQuestion = 0;
+    correctScoreElement.textContent=correctScore;
+    totalQuestionsElement.textContent= currentQuestion + "/" + totalQuestions;
+
+    RandomCities();
+  }
+
+  async function RandomCities(){
+    alert ('le jeu commence');
+  }
+
+  confirmButton.addEventListener('click',() => {
+
+    initGame();
+  });
+
 });
