b0714afdecc716d5ec6633e987a865f9c3d15743
Hikki-ko <arcobalenosensei@gmail.com>
2025-10-24 23:18:00 +0400

    Ajout de la page d'introduction au quizz. Amélioration CSS. Simplification du header.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,56 +8,81 @@ const API_KEY =
  *	chargés et existants dans notre DOM.
  **************/
 window.addEventListener("DOMContentLoaded", (event) => {
-  // Pseudo-code !!!
-  //const nombre_questions = "valeur récupérée dans l'HTML";
-  //const liste_villes =
-  "https://api.openaq.org/v3/locations?limit=${nombre_questions}";
+  // ### Page d'introduction ###
+  document.getElementById("introduction_content").hidden = false;
+  const startButton = document.querySelector("#startButton");
 
-  //On récupère les éléments du DOM
-  const confirmButton = document.querySelector(".button");
-  const cityItems = document.querySelectorAll(".city_item");
-  const correctScoreElement = document.getElementById("correctScore");
-  const totalQuestionsElement = document.getElementById("totalQuestions");
-  const radioSelect = document.querySelectorAll('input[type="radio"]');
-  const musiqueFond = document.querySelector("#musiqueFond");
+  // Fonction sélecteur du nombre de questions
+  let selector = document.querySelector(".selector");
+  let selectValue = document.querySelector(".select_value");
+  let sliderInput = document.querySelector("#slider_input");
+  let progressBar = document.querySelector(".progress_bar");
+  let numberOfQuestions;
 
-  //On définit les données du jeu
-  let correctScore = 0;
-  let totalQuestions = 10;
-  let currentQuestion = 0;
-  let cityData = []; //pour stocker les données des villes de l'API
+  selectValue.innerText = sliderInput.value;
+  progressBar.style.width = `${sliderInput.value}%`;
 
-  function initGame() {
-    correctScore = 0;
-    currentQuestion = 0;
-    correctScoreElement.textContent = correctScore;
-    totalQuestionsElement.textContent = currentQuestion + "/" + totalQuestions;
-
-    RandomCities();
-  }
-
-  async function RandomCities() {
-    alert("le jeu commence");
-  }
-
-  confirmButton.addEventListener("click", () => {
-    initGame();
+  sliderInput.oninput = () => {
+    let value = sliderInput.value;
+    selector.style.left = `${value}%`;
+    selectValue.innerText = value;
+    progressBar.style.width = `${value}%`;
+  };
+  startButton.addEventListener("click", () => {
+    // Enregistrement du nombre de questions
+    numberOfQuestions = sliderInput.value;
+    const numberOfCities = `https://api.openaq.org/v3/locations?limit=${numberOfQuestions}`;
+    // Remplacement de la page d'introduction
+    document.getElementById("introduction_content").classList.add("hidden");
+    document.getElementById("quiz_container").classList.remove("hidden");
   });
 
-  // Effet sonore de click sur le formulaire
-  document.addEventListener("click", () => {
-    const clickSound = new Audio("assets/audio/clic_radio.mp3");
-    clickSound.volume = 0.3;
-    clickSound.play();
-  });
+  // Démarrage du quizz
+  // function initGame() {
+  //   correctScore = 0;
+  //   currentQuestion = 0;
+  //   correctScoreElement.textContent = correctScore;
+  //   totalQuestionsElement.textContent = currentQuestion + "/" + totalQuestions;
 
-  // Musique de fond sur premier clic
-  document.addEventListener(
-    "click",
-    () => {
-      musiqueFond.volume = 0.2;
-      musiqueFond.play();
-    },
-    { once: true }
-  );
+  //   RandomCities();
+  // }
+
+  // async function RandomCities() {
+  //   alert("le jeu commence");
+  // }
+
+  // confirmButton.addEventListener("click", () => {
+  //   initGame();
+  // });
+
+  // //On récupère les éléments du DOM
+  // const confirmButton = document.querySelector(".button");
+  // const cityItems = document.querySelectorAll(".city_item");
+  // const correctScoreElement = document.getElementById("correctScore");
+  // const totalQuestionsElement = document.getElementById("totalQuestions");
+  // const radioSelect = document.querySelectorAll('input[type="radio"]');
+  // const musiqueFond = document.querySelector("#musiqueFond");
+
+  // //On définit les données du jeu
+  // let correctScore = 0;
+  // let totalQuestions = 10;
+  // let currentQuestion = 0;
+  // let cityData = []; //pour stocker les données des villes de l'API
+
+  // // Effet sonore de click sur le formulaire
+  // document.addEventListener("click", () => {
+  //   const clickSound = new Audio("assets/audio/clic_radio.mp3");
+  //   clickSound.volume = 0.3;
+  //   clickSound.play();
+  // });
+
+  // // Musique de fond sur premier clic
+  // document.addEventListener(
+  //   "click",
+  //   () => {
+  //     musiqueFond.volume = 0.2;
+  //     musiqueFond.play();
+  //   },
+  //   { once: true }
+  // );
 });
