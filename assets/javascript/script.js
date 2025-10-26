f4157b41bb03d59f50338a4a6b1830d5b5994376
Zoé <zoe.van-de-moortele@etu.unilim.fr>
2025-10-26 16:59:01 +0100

    git commit -m 'Dev JS fonction fetch'

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,6 @@
 // Déclarations variables
 const API_KEY =
-  "63c89bc4a68f3733d17ec87750e68ab04de872ba71f88f7703f2ea0161502834";
+  "28ac8a70505094e8eaabd8e9386a5df44fae18cc3ede6a261a52ca073dc58b62";
 
 /**************
  *	On ajoute un évènement "lorsque l'arborescence DOM est chargée".
@@ -12,12 +12,18 @@ window.addEventListener("DOMContentLoaded", (event) => {
   document.getElementById("introduction_content").hidden = false;
   const startButton = document.querySelector("#startButton");
 
+  //Variables globales
+  let numberOfQuestions;
+  let cityData = []; //pour stocker les données des villes de l'API
+  let currentcities = []; //stock les deux villes de la question actuelle
+  let currentQuestion = 1;
+  let correctScore = 0;
+
   // Fonction sélecteur du nombre de questions
   let selector = document.querySelector(".selector");
   let selectValue = document.querySelector(".select_value");
   let sliderInput = document.querySelector("#slider_input");
   let progressBar = document.querySelector(".progress_bar");
-  let numberOfQuestions;
 
   selectValue.innerText = sliderInput.value;
   progressBar.style.width = `${sliderInput.value}%`;
@@ -31,44 +37,62 @@ window.addEventListener("DOMContentLoaded", (event) => {
   startButton.addEventListener("click", () => {
     // Enregistrement du nombre de questions
     numberOfQuestions = sliderInput.value;
-    const numberOfCities = `https://api.openaq.org/v3/locations?limit=${numberOfQuestions}`;
+    const numberOfCities = fetchCities(numberOfQuestions * 2);
     // Remplacement de la page d'introduction
     document.getElementById("introduction_content").classList.add("hidden");
     document.getElementById("quiz_container").classList.remove("hidden");
     document.getElementById("bg_music").play();
   });
 
-  // Démarrage du quizz
-  // function initGame() {
-  //   correctScore = 0;
-  //   currentQuestion = 0;
-  //   correctScoreElement.textContent = correctScore;
-  //   totalQuestionsElement.textContent = currentQuestion + "/" + totalQuestions;
+  async function fetchCities(numberOfCities) {
+    const url = `https://api.openaq.org/v3/locations?limit=${numberOfCities}&order_by=random`;
 
-  //   RandomCities();
-  // }
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'X-API-Key': '28ac8a70505094e8eaabd8e9386a5df44fae18cc3ede6a261a52ca073dc58b62',
+          'Origin': 'http://127.0.0.1:5500'  // Explicite l'origine
+        },
+        mode: 'cors',  // Force le mode CORS
+      });
 
-  // async function RandomCities() {
-  //   alert("le jeu commence");
-  // }
+      if (!response.ok) {
+        throw new Error(`Erreur: ${response.status} - ${response.statusText}`);
+      }
 
-  // confirmButton.addEventListener("click", () => {
-  //   initGame();
-  // });
+      const data = await response.json(); //On convertit les données en objet JSON
+      console.log("Données API reçues:", data.results);
 
-  // //On récupère les éléments du DOM
-  // const confirmButton = document.querySelector(".button");
-  // const cityItems = document.querySelectorAll(".city_item");
-  // const correctScoreElement = document.getElementById("correctScore");
-  // const totalQuestionsElement = document.getElementById("totalQuestions");
-  // const radioSelect = document.querySelectorAll('input[type="radio"]');
-  // const musiqueFond = document.querySelector("#musiqueFond");
+      // Filtrer les villes avec des données pm25 valides - données utilisés pour comparer les taux de pollution
+      cityData = data.results.filter(city =>
+        city.parameters && city.parameters.pm25 && city.parameters.pm25.length > 0
+      );
+
+      //On vérigie qu'on a assez de villes après le filtrage
+      if (cityData.length<numberOfCities){
+        alert('Pas assez de villes pour commencer le quizz, veuillez choisir moins de questions')
+        return fetchCities(numberOfCities);
+      };
+
+      console.log('villes filtrées :', cityData);
+      return cityData.slice(0, numberOfCities);
+
+    } catch (error) {
+      console.error("Erreur dans fetchCities:", error);
+      alert("Impossible de récupérer les villes. Vérifiez votre connexion ou réessayez plus tard.");
+      return null; 
+    }
+  }
+
+
+  
 
   // //On définit les données du jeu
   // let correctScore = 0;
   // let totalQuestions = 10;
   // let currentQuestion = 0;
-  // let cityData = []; //pour stocker les données des villes de l'API
+  
 
   // // Effet sonore de click sur le formulaire
   // document.addEventListener("click", () => {
